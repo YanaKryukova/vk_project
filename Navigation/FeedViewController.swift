bbcbf98dcc3cd41a537ed74ff223ed9f235b2fbc
Yana Kryukova <kruikova10@gmail.com>
2023-04-12 17:25:08 +0300

    first commit

---
+++ Navigation/FeedViewController.swift
@@ -0,0 +1,39 @@
+//
+//  FeedViewController.swift
+//  Navigation
+//
+//  Created by Яна Крюкова on 06.04.2023.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemTeal
+        title = "Feed"
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        
+        let button = UIButton(type: .system)
+        button.frame = CGRect(origin: CGPoint(x: 120, y: 120), size: CGSize(width: 150, height: 30))
+        button.setTitle("Post", for: .normal)
+        button.backgroundColor = .systemPink
+        
+        view.addSubview(button)
+        button.addTarget(self, action: #selector(post), for: .touchUpInside)
+    }
+    
+    @objc func post() {
+        
+        let postViewController = PostViewController()
+        postViewController.someInfo = "some info"
+        
+        navigationController?.pushViewController(postViewController, animated: true)
+    }
+}
+
---
+++ Navigation/PostStructure.swift
@@ -0,0 +1,13 @@
+//
+//  PostStructure.swift
+//  Navigation
+//
+//  Created by Яна Крюкова on 09.04.2023.
+//
+
+import Foundation
+
+struct Post {
+    
+    var title: String
+}
